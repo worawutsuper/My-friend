0b0662afb91023111952c3b0110c35499aa4aa87
wut <wutclub117@gmail.com>
2016-09-17 12:37:22 +0700

    start appเริ่ม แอพ

---
+++ app/src/main/java/studuumg/wutapp/myfriend/MainActivity.java
@@ -0,0 +1,20 @@
+package studuumg.wutapp.myfriend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }   //Main Method
+
+ public void clickSignUpMain(View view){
+     startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+ }
+
+}  //Main Class
---
+++ app/src/main/java/studuumg/wutapp/myfriend/SignUpActivity.java
@@ -0,0 +1,16 @@
+package studuumg.wutapp.myfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+
+}// Main Class
