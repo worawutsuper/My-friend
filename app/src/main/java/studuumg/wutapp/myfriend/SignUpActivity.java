b6157c5ad0bccdce05b991ed08751b7624ec5baf
wut <wutclub117@gmail.com>
2016-09-18 12:46:01 +0700

    up image finish

--- app/src/main/java/studuumg/wutapp/myfriend/SignUpActivity.java
+++ app/src/main/java/studuumg/wutapp/myfriend/SignUpActivity.java
@@ -1,11 +1,13 @@
 package studuumg.wutapp.myfriend;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +16,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -148,11 +154,37 @@ public class SignUpActivity extends AppCompatActivity {
             uploadImageToServer();
 
         }
-
-
     }   // clickSign
 
     private void uploadImageToServer() {
+
+
+        //New Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try{
+
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com",21,
+                    "18Sep@swiftcodingthai.com","Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+            Log.d("MyfrindV1","upload Finish");
+
+
+
+        } catch (Exception e) {
+            //e.printStackTrace();
+            Log.d("Myfriendv1","e==>"+e.toString());
+        }
+
+
+
 
 
 
